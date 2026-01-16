bf862a4a0d06afb30d1df3773d1358a45dc2e403
DaBlower <113282214+DaBlower@users.noreply.github.com>
2026-01-17 08:42:55 +1100

    fix previous issue
    there's still one with the arguments though

--- src/commands/custom_message.py
+++ src/commands/custom_message.py
@@ -2,6 +2,7 @@
 import os
 import logging
 import datetime
+from slack_sdk.errors import SlackApiError
 
 program_name = "custom_message" # change this to the name of the command
 
@@ -16,28 +17,50 @@ operators = None
 
 def custom_message_handler(ack, respond, command):
     ack()
-    name = "/sticky-note"
+    print("ack")
+    name = "/custom-message"
 
     channel_id = command["channel_id"]
     user_id = command["user_id"]
     args = command["text"].split(" ") # action, rest
 
+    print("op test")
     if user_id not in operators:
         respond(text="You need to be an operator of this bot to use this command :(", response_type="ephemeral")
         logger.info(f"User {user_id} used tried to use command {name} in {channel_id} without sufficient permissions")
+        print("not in op")
         return
     
+    print("arg test")
     if len(args) < 1 or len(args) > 3:
-        respond(text="Usage: `/custom-message <message> <pfp link (optional)> <name of the bot (optional)>`", response_type="ephemeral")
+        respond(text='Usage: `/custom-message <name of the bot (optional) or "/self"> <pfp link (optional)> <message>`", response_type="ephemeral')
+        print("args not long enough")
         return
 
     message = args[0]
+    print("msg done")
+
+    # check if \self is used for username, and copy the users pfp and username
+
+    username = args[1] if len(args) > 1 else None
     
-    pfp = (args[1] if args[1] else None)
+    if username == "/self":
+        print("user did self")
+        profile = app.client.users_profile_get(user = user_id) # get data
+        username = profile["display_name"]
+        pfp = profile["image_192.png"]
 
-    username = (args[2] if args[2] else None)
 
-    app.client.chat_postMessage(channel=channel_id, icon_url=pfp if pfp else None, username=username if username else None, markdown_text=message)
+    else:
+        pfp = args[2] if len(args) > 2 else None 
+        print("pfp done")
+
+    print(f"post message message={message}, pfp={pfp}, username={username}")
+    try:
+        app.client.chat_postMessage(channel=channel_id, username=username if username else None, icon_url=pfp if pfp else None, text=message)
+    except SlackApiError as e:
+        logger.error(f"Error sending message: {e}")
+
     return
 
 def initialise_custom_message(slack_app):
@@ -77,4 +100,4 @@ def initialise_custom_message(slack_app):
     logger.info(f"{program_name} initialised")
     
     # Register command handler
-    app.command("/custom_message")(custom_message_handler)
+    app.command("/custom-message")(custom_message_handler)
